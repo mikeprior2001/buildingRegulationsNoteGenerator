5b4232b9ac401e32129a5d03bebcc59b536fc4a9
Mike Prior <mikeprior2001@gmail.com>
2022-08-02 21:42:15 +0100

    Initial Commit

---
+++ Building Regulator Note Generator/Building_Regulator_Note_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Building_Regulator_Note_GeneratorApp.swift
+//  Building Regulator Note Generator
+//
+//  Created by Mike Prior on 02/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Building_Regulator_Note_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Building Regulator Note Generator/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Building Regulator Note Generator
+//
+//  Created by Mike Prior on 02/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
